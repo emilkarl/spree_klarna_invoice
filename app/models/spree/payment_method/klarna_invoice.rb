44642b45a09d4c78e70e482a9971a95b5434aa32
Emil Karlsson <emil@nocweb.se>
2012-03-28 14:23:27 +0200

    Cleaned up payment method

--- app/models/spree/payment_method/klarna_invoice.rb
+++ app/models/spree/payment_method/klarna_invoice.rb
@@ -3,12 +3,24 @@ class Spree::PaymentMethod::KlarnaInvoice < Spree::PaymentMethod
   preference :store_id, :integer
   preference :store_secret, :string
   
-  def source_required?
-    true
+  def actions
+    %w{capture}
   end
   
-  def payment_source_class
-    Spree::KlarnaPayment
+  def require_source?
+    false
+  end
+  
+  # Indicates whether its possible to capture the payment
+  def can_capture?(payment)
+    ['checkout', 'pending'].include?(payment.state) #&& payment.order.klarna_invoice_number.blank?
+  end
+
+  def capture(payment)
+    logger.info "\n\n\n------------------ CAPTURE ------------------\n"
+    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
+    payment.complete
+    true
   end
 end
 
