1c1e4bae52f56554f4d49c5e3e4e56253b25c052
Emil Karlsson <emil@nocweb.se>
2012-09-17 14:39:02 +0200

    Added SSO number validation as config param

--- app/helpers/base_helper_decorator.rb
+++ app/helpers/base_helper_decorator.rb
@@ -2,4 +2,12 @@ Spree::BaseHelper.class_eval do
   def get_client_ip
     request.remote_ip
   end
+  
+  def pnr_validation_error(min, max)
+    if(min != max) 
+      "#{I18n.t(:pnr_validation_first)} #{I18n.t(:between)} #{Spree::PaymentMethod::KlarnaInvoice.new.preferred(:pnr_min)} #{I18n.t(:and)} #{Spree::PaymentMethod::KlarnaInvoice.new.preferred(:pnr_max)} #{I18n.t(:chars)}. #{I18n.t(:pnr_formats)} #{Spree::PaymentMethod::KlarnaInvoice.new.preferred(:pnr_formats)}"
+    else
+      "#{I18n.t(:pnr_validation_first)} #{Spree::PaymentMethod::KlarnaInvoice.new.preferred(:pnr_min)} #{I18n.t(:chars)}. #{I18n.t(:pnr_formats)} #{Spree::PaymentMethod::KlarnaInvoice.new.preferred(:pnr_formats)}"
+    end
+  end
 end
